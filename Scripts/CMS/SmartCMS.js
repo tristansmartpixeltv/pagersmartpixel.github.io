cb5f4657b546aed8abc70f73bf8c2833069cf6ac
tristansmartpixeltv <40502490+tristansmartpixeltv@users.noreply.github.com>
2018-09-28 11:43:11 -0400

    Fixed with github name

--- Scripts/CMS/SmartCMS.js
+++ Scripts/CMS/SmartCMS.js
@@ -7,7 +7,7 @@ var smartCms = function(ProjectID,Host,ProjectEnvironment,Version,DataTableNames
     this.Version = Version;
     this.DatatableNames = DataTableNames;
 
-    this.path = "../../JSON/"
+    this.path = "pagersmartpixel.github.io/../JSON/"
 
    
 }
