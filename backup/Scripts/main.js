4e64c1818ce968422c55517bde2f46aa9970f6b2
tristansmartpixeltv <40502490+tristansmartpixeltv@users.noreply.github.com>
2018-12-20 14:48:44 -0500

    added backup

---
+++ backup/Scripts/main.js
@@ -0,0 +1,143 @@
+var maxScroll = window.screen.height * 2;
+
+var MainParallaxScene = null;
+
+var LocalizationM = null;
+
+var CMS;
+
+
+
+
+window.onload = function(){
+
+	CMS =  new smartCms('51b026a3-ee60-4fb4-b8c5-c673ca10cd5b','https://smartcms.azurewebsites.net',2,2,new Array("LocalizedTexts"));
+
+	CMS.LoadLocalCache(function(table,tableName){
+        LocalizationM = new LocalizationManager(table[0].Rows,'Key','Text','en',new Array("fr","en","default"));
+    });
+
+	GoToSectionHome();
+
+	MainParallaxScene = new ParallaxScene();
+	
+	setVideo();
+
+	
+	
+	
+	if(isUsingInternetExplorer())
+	{
+			var websiteContactContainer = document.getElementById("content-table");
+			var websiteTitle = document.body.getElementsByClassName('section-contact-maintitle title-bold')[0];
+			websiteTitle.style.top = '257vh';
+			websiteContactContainer.style.top = '257vh';
+	}
+
+
+	
+	StartScreenSaver(function(){
+
+		if(!isUsingInternetExplorer())
+		{
+			if(window.screen.width > 767)
+				MainParallaxScene.start();
+		}
+
+	});
+};
+
+
+function isUsingInternetExplorer(){
+
+
+
+  if (/MSIE 10/i.test(navigator.userAgent)) {
+      return true;
+  }
+
+  if (/MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
+      return true;
+  }
+
+  if (/Edge\/\d./i.test(navigator.userAgent)){
+     return true;
+  }
+
+  return false;
+
+}
+
+
+function GoToSectionHome(){
+	ScrollTo(0);
+}
+
+
+function GoToSectionAboutUs(){
+	if(!isUsingInternetExplorer())
+		ScrollTo($("#section-about").offset().top - (window.innerHeight/2) + 100);
+	else
+		ScrollTo($("#section-about").offset().top);
+}
+
+function GoToSectionContact(){
+	if(!isUsingInternetExplorer())
+		ScrollTo($(".section-contact-maintitle").offset().top - (window.innerHeight / 2));
+	else
+		ScrollTo($(".section-contact-maintitle").offset().top - 100);
+}
+
+
+function ScrollTo(topPosition){
+
+	if(!isUsingInternetExplorer())
+	{
+		window.scroll({
+  		top: topPosition, 
+  		left: 0, 
+  		behavior: 'smooth' 
+		});
+
+	}
+	else
+	{
+		window.scrollTo(0,topPosition);
+	}
+}
+
+function ChangeApplicationLanguage(){
+
+	if(CurrentLanguage != 'en')
+	{
+		LocalizationM.changeLanguage('en');
+	}
+	else
+	{
+		LocalizationM.changeLanguage('fr');
+	}
+
+	
+	
+}
+
+
+function setVideo(){
+	var MainVideoHtmlElement = document.getElementById('section-about-video');
+	var MainVideoSourceHtmlElement = MainVideoHtmlElement.getElementsByTagName('source')[0];
+	
+	if(window.screen.width <= 1024){
+		MainVideoSourceHtmlElement.setAttribute('src','https://tristansmartpixeltv.github.io/pagersmartpixel.github.io/Videos/Showreel-SMPX-480p-Mobile.mp4');
+		MainVideoSourceHtmlElement.setAttribute('controls','controls');
+	}
+    else
+	{
+		MainVideoSourceHtmlElement.setAttribute('src','https://tristansmartpixeltv.github.io/pagersmartpixel.github.io/Videos/Showreel-SMPX-480p.mp4');
+	}
+	
+}
+
+
+
+
+
---
+++ backup/Scripts/screensaver.js
@@ -0,0 +1,68 @@
+var maintitle = null;
+var screensaver = null;
+
+function StartScreenSaver(callback){
+		
+		maintitle = document.getElementById('main-title');
+		screensaver = document.getElementById('section-screensaver');
+		
+		 maintitle.classList.add('typerwriter-animation');
+		 
+		 GoToSectionHome();
+		 disableScroll();
+		 
+		 setTimeout(function(){
+		 	EndScreenSaver(callback);
+		 },4500);
+}
+
+
+
+function EndScreenSaver (callback){
+	screensaver.classList.add('section-screensaver-disabled');
+	enableScroll();
+	callback();
+}
+
+
+//Disable Scroll
+
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
+
+
+	
